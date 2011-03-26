cb878604d8babeea5c5b5310190920feb04071f8
Milan Laslop <milan33@gmail.com>
2011-03-26 17:08:45 +0100

    [Data] changed nested graph sphere radius (for testing) [+] added simple nested graph file

--- src/Data/Graph.cpp
+++ src/Data/Graph.cpp
@@ -295,7 +295,7 @@ void Data::Graph::createNestedGraph(osg::ref_ptr<Data::Node> srcNode)
 
 void Data::Graph::closeNestedGraph()
 {
-	QSharedPointer<Layout::ShapeGetter> shapeGetter (new Layout::ShapeGetter_Sphere_AroundNode (this->parent_id.last(), 100));
+	QSharedPointer<Layout::ShapeGetter> shapeGetter (new Layout::ShapeGetter_Sphere_AroundNode (this->parent_id.last(), 10));
 	restrictionsManager_.setRestrictions (this->nestedNodes, shapeGetter);
 
 	//this->getRestrictionsManager().setRestrictions(
