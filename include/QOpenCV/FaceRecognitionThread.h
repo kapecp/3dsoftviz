5c2bd93dfad7bce29b81e4cc6f4fa56bf3b6035e
David <yodadado@gmail.com>
2013-11-23 22:35:19 +0100

    [FaceRecognitionThread] change params of signal sendEyesCoord to float
    Updating parameters of signal sendEyesCoord(). They were change to float.
    Adding third parameter distance for future usage and also compatibility
    with slot setRotationHead in CameraManipulator.

--- include/QOpenCV/FaceRecognitionThread.h
+++ include/QOpenCV/FaceRecognitionThread.h
@@ -11,43 +11,43 @@ Q_DECLARE_METATYPE(cv::Mat);
 
 namespace QOpenCV
 {
-    class FaceRecognitionWindow;
+	class FaceRecognitionWindow;
 
-    /**
-     * @brief Class FaceRecognitionThread
-     * @author Autor: Marek Jakab
-     * @date 18.11.2013
-     */
-    class FaceRecognitionThread : public QThread
+	/**
+	 * @brief Class FaceRecognitionThread
+	 * @author Autor: Marek Jakab
+	 * @date 18.11.2013
+	 */
+	class FaceRecognitionThread : public QThread
 	{
-        Q_OBJECT
+		Q_OBJECT
 
 	public:
-        FaceRecognitionThread(OpenCV::FaceRecognizer *alg);
-        ~FaceRecognitionThread(void);
-        OpenCV::FaceRecognizer *mFaceRecognizer;
-        OpenCV::CapVideo *mCapVideo;
+		FaceRecognitionThread(OpenCV::FaceRecognizer *alg);
+		~FaceRecognitionThread(void);
+		OpenCV::FaceRecognizer *mFaceRecognizer;
+		OpenCV::CapVideo *mCapVideo;
 		FaceRecognitionWindow *mFaceRecognitionWindow;
 
-        /**
-         * @author Autor: Marek Jakab
-         * @brief setWindow Pass FaceRecognitionWindow instance param to thread
-         * @param mFaceRecognitionWindow instance of window for recognition
-         */
-        void setWindow(QOpenCV::FaceRecognitionWindow *mFaceRecognitionWindow);
+		/**
+		 * @author Autor: Marek Jakab
+		 * @brief setWindow Pass FaceRecognitionWindow instance param to thread
+		 * @param mFaceRecognitionWindow instance of window for recognition
+		 */
+		void setWindow(QOpenCV::FaceRecognitionWindow *mFaceRecognitionWindow);
 
-        /**
-         * @author Autor: Marek Jakab
-         * @brief run Starts thread
-         */
-        void run();
-        signals:
-            /**
-             * @author Autor: Marek Jakab
-             * @brief pushImage Send image to FaceRecognitionWindow
-             * @param Image cv::Mat
-             */
-            void pushImage(cv::Mat Image);
+		/**
+		 * @author Autor: Marek Jakab
+		 * @brief run Starts thread
+		 */
+		void run();
+		signals:
+			/**
+			 * @author Autor: Marek Jakab
+			 * @brief pushImage Send image to FaceRecognitionWindow
+			 * @param Image cv::Mat
+			 */
+			void pushImage(cv::Mat Image);
 
 			/**
 			 * @author Autor: Marek Jakab
@@ -55,20 +55,20 @@ namespace QOpenCV
 			 * @param x % distance from middle on X axis
 			 * @param y % distance from middle on Y axis
 			 */
-			void sendEyesCoords(int x, int y);
+			void sendEyesCoords(float x, float y, float distance);
 	private:
-            bool cancel;
-    private slots:
-            /**
-             * @author Autor: Marek Jakab
-             * @brief setCancel Sets cancel=true
-             */
+			bool cancel;
+	private slots:
+			/**
+			 * @author Autor: Marek Jakab
+			 * @brief setCancel Sets cancel=true
+			 */
 			void setCancel(bool);
-            /**
-             * @author Autor: Marek Jakab
-             * @brief pauseWindow Pauses recognition window
-             */
-            void pauseWindow();
+			/**
+			 * @author Autor: Marek Jakab
+			 * @brief pauseWindow Pauses recognition window
+			 */
+			void pauseWindow();
 	};
 }
 
--- src/QOpenCV/FaceRecognitionThread.cpp
+++ src/QOpenCV/FaceRecognitionThread.cpp
@@ -4,10 +4,10 @@ using namespace QOpenCV;
 
 QOpenCV::FaceRecognitionThread::FaceRecognitionThread(OpenCV::FaceRecognizer* alg)
 {
-    this->mFaceRecognizer = alg;
+	this->mFaceRecognizer = alg;
 	this->mCapVideo = new OpenCV::CapVideo(0,320,240);
 	this->cancel=false;
-    qRegisterMetaType<cv::Mat>("Mat");
+	qRegisterMetaType<cv::Mat>("Mat");
 }
 
 QOpenCV::FaceRecognitionThread::~FaceRecognitionThread(void)
@@ -18,27 +18,28 @@ QOpenCV::FaceRecognitionThread::~FaceRecognitionThread(void)
 
 void QOpenCV::FaceRecognitionThread::setWindow(FaceRecognitionWindow *mFaceRecognitionWindow)
 {
-    this->mFaceRecognitionWindow = mFaceRecognitionWindow;
+	this->mFaceRecognitionWindow = mFaceRecognitionWindow;
 	connect(this->mFaceRecognitionWindow,SIGNAL(cancelLoop(bool)),this,SLOT(setCancel(bool)));
 }
 
 void QOpenCV::FaceRecognitionThread::run()
 {
-    cv::Mat image;
+	cv::Mat image;
 	while(!cancel) {
 		image=this->mCapVideo->queryFrame();
-        this->mCapVideo->createGray();
+		this->mCapVideo->createGray();
 
-        this->mFaceRecognizer->detectFaces(this->mCapVideo->getGrayframe());
-        this->mFaceRecognizer->annotateFaces(image);
+		this->mFaceRecognizer->detectFaces(this->mCapVideo->getGrayframe());
+		this->mFaceRecognizer->annotateFaces(image);
 		cv::Mat im = image.clone();
 		emit this->pushImage(im);
 		if (this->mFaceRecognizer->detected)
 		{
 //			emit this->sendEyesCoords(this->mFaceRecognizer->getEyesCoords().x,
-//									  this->mFaceRecognizer->getEyesCoords().y);
+//									  this->mFaceRecognizer->getEyesCoords().y,
+//									  0.0 );
 		}
-    }
+	}
 }
 
 void QOpenCV::FaceRecognitionThread::pauseWindow()
