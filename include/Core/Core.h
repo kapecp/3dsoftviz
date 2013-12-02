f6aa9e452d1c6bfd122d0ac0c30febad3158f825
TheWarek <marko.jakab@gmail.com>
2013-12-02 15:46:27 +0100

    [Fix] code formatting

--- include/Core/Core.h
+++ include/Core/Core.h
@@ -19,94 +19,94 @@
 
 namespace QOSG
 {
-    class CoreWindow;
+class CoreWindow;
 }
 
 namespace AppCore
 {
 
-    /**
-     * \class Core
-     * \brief Core control whole application
-     *
-     * Class is implemented as singleton. Core provides functionality to work with layout threads, application windows and core graph.
-     *
-     * \author Pavol Perdik
-     * \date 8.5.2010
-     */
-    class Core
-    {
-    public:
-        ~Core();
+/**
+	 * \class Core
+	 * \brief Core control whole application
+	 *
+	 * Class is implemented as singleton. Core provides functionality to work with layout threads, application windows and core graph.
+	 *
+	 * \author Pavol Perdik
+	 * \date 8.5.2010
+	 */
+class Core
+{
+public:
+	~Core();
 
-        /**
-         * \fn restartLayout
-         * \brief Restarts layouting of graph.
-         */
-        void restartLayout();
+	/**
+		 * \fn restartLayout
+		 * \brief Restarts layouting of graph.
+		 */
+	void restartLayout();
 
-        /**
-         * \fn getLayoutThread
-         * \brief Returns Layout::LayoutThread
-         */
-        Layout::LayoutThread* getLayoutThread(){return this->thr;}
+	/**
+		 * \fn getLayoutThread
+		 * \brief Returns Layout::LayoutThread
+		 */
+	Layout::LayoutThread* getLayoutThread(){return this->thr;}
 
-        /**
-         * \fn getInstance
-         * \brief Returns instance.
-         */
-        static Core* getInstance(QApplication * app = 0);
+	/**
+		 * \fn getInstance
+		 * \brief Returns instance.
+		 */
+	static Core* getInstance(QApplication * app = 0);
 
-        /**
-         * QOSG::MessageWindows * messageWindows;
-         * \brief Provides functionality to show information or error popup windows and progress bar.
-         */
-        QOSG::MessageWindows * messageWindows;
+	/**
+		 * QOSG::MessageWindows * messageWindows;
+		 * \brief Provides functionality to show information or error popup windows and progress bar.
+		 */
+	QOSG::MessageWindows * messageWindows;
 
-        /**
-         * Layout::LayoutThread * thr
-         * \brief Thread of Layout algorithm.
-         */
-        Layout::LayoutThread * thr;
+	/**
+		 * Layout::LayoutThread * thr
+		 * \brief Thread of Layout algorithm.
+		 */
+	Layout::LayoutThread * thr;
 
-        /**
-          * \fn getCoreWindow
-          * \brief Returns QOSG::CoreWindow
-          **/
-        QOSG::CoreWindow* getCoreWindow(){return this->cw;}
-    private:
-        /**
-        *  QOSG::CoreWindow * cw
-        *  \brief instance of CoreWindow
-        */
-        QOSG::CoreWindow * cw;
+	/**
+		  * \fn getCoreWindow
+		  * \brief Returns QOSG::CoreWindow
+		  **/
+	QOSG::CoreWindow* getCoreWindow(){return this->cw;}
+private:
+	/**
+		*  QOSG::CoreWindow * cw
+		*  \brief instance of CoreWindow
+		*/
+	QOSG::CoreWindow * cw;
 
-        /**
-        *  \fn private constructor  GraphManager(QApplication * app)
-        *  \brief
-        *  \param app 
-        */ 
-       Core(QApplication * app);
+	/**
+		*  \fn private constructor  GraphManager(QApplication * app)
+		*  \brief
+		*  \param app
+		*/
+	Core(QApplication * app);
 
-        /**
-        *  Layout::FRAlgorithm * alg
-        *  \brief instance of layout algorithm.
-        */
-        Layout::FRAlgorithm * alg;
+	/**
+		*  Layout::FRAlgorithm * alg
+		*  \brief instance of layout algorithm.
+		*/
+	Layout::FRAlgorithm * alg;
 
 
-        /**
-        *  Vwr::CoreGraph * cg
-        *  \brief instance of CoreGraph
-        */
-        Vwr::CoreGraph * cg;
+	/**
+		*  Vwr::CoreGraph * cg
+		*  \brief instance of CoreGraph
+		*/
+	Vwr::CoreGraph * cg;
 
-        /**
-        *  Core * core;
-        *  \brief instance of this class
-        */
-        static Core * core;
-    };
+	/**
+		*  Core * core;
+		*  \brief instance of this class
+		*/
+	static Core * core;
+};
 }
 
 #endif
