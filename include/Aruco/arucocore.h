8addb71ff13711d4ac7718ae87007bbd4e8f07d6
whitelilit <whitelilit@whitelilit-PC.(none)>
2013-12-16 21:43:22 +0100

    Added first initial version of ArucoCore class

---
+++ include/Aruco/arucocore.h
@@ -0,0 +1,59 @@
+#ifndef ARUCOCORE_H
+#define ARUCOCORE_H
+
+#include <aruco/aruco.h>
+#include <aruco/cvdrawingutils.h>
+
+class ArucoCore : public QObject
+{
+    /**
+        *@brief Class ArucoCore
+        *@author Autor: Martina Tregerova
+        *@date 16.12.2013
+        *@note This is the first draft version of class. Most functions will be changed in next few weeks.
+        */
+public:
+    /**
+         * @author Autor:Martina Tregerova
+         * @brief Constructor
+         * @note Will be changed soon.
+         */
+    ArucoCore();
+    /**
+         * @author Autor: Martina Tregerova
+         * @brief getMatrix Fills the field with information about marker position given from aruco.
+         * @param modelviewmatrix double* the field that needs to be filled
+         * @note The need to send a field will be changed, too.
+         */
+    int getMatrix(double* modelviewmatrix);
+    /**
+         * @author Autor: Martina Tregerova
+         * @brief updateImage updates the camera matrix with each update.
+         * @param ImputImage cv::Mat the camera matrix
+         * @note The need to send the image will be changed too.
+         */
+    void updateImage(cv::Mat InputImage);
+    /**
+         * @author Autor: Martina Tregerova
+         * @brief detectMarkers The actual function that creates a position information based on camera information.
+         */
+    void detectMarkers();
+    /**
+         * @author Autor: Martina Tregerova
+         * @brief cameraParameters Function that reads the camera information from yml file.
+         * @note May not be needed anymore. Also the file will not be found.
+         */
+    void cameraParameters();
+
+private:
+    cv::Mat CameraImage;
+    aruco::CameraParameters Camera;
+    aruco::MarkerDetector MDetector;
+    vector<aruco::Marker> Markers;
+    float MarkerSize;
+
+//signals:
+    //detect();
+};
+
+#endif // ARUCOCORE_H
