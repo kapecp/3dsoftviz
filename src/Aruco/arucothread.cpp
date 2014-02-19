f8a6983ebeaffc30d43449de0aafc9a1e3342d9f
durcakd <durcak01.david@gmail.com>
2014-02-19 12:24:13 +0100

    [ArucoThread] add basic example of run method. Its only example and must be changed soon

--- src/Aruco/arucothread.cpp
+++ src/Aruco/arucothread.cpp
@@ -1,5 +1,13 @@
 #include "Aruco/arucothread.h"
 
+#include "opencv2/core/core.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "Aruco/arucocore.h"
+#include <QFileInfo>
+#include <QDebug>
+
 using namespace ArucoModul;
 
 ArucoThread::ArucoThread(void)
@@ -12,12 +20,52 @@ ArucoThread::~ArucoThread(void)
 }
 
 
+
 void ArucoThread::run()
 {
+	// this code will be changed soon
+	cv::Mat frame;
+	CvCapture *capture;
+	QMatrix4x4 mat;
+	QString filename = "../share/3dsoftviz/config/GrafBoard.yml";
+
+	// this must do camera singleton
+	capture = cvCreateCameraCapture(1);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 300);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 200);
+
+	QFileInfo file(filename);
+	if( ! file.exists() ){
+		qDebug() << "File " << file.absoluteFilePath() << " does Not exist!";
+	}
+	ArucoCore aCore(filename);
+
+
 	while(!cancel) {
+		//for(int i=0; i<2000; i++ ) {
 		// doing aruco work in loop
+		// get image from camera
+		// add image to aruco and get matrix
+		// emit matrix
+
+
+		frame = cvQueryFrame(capture);
+		cv::imshow("face", frame);
+		cv::waitKey(1);
+		mat = aCore.getDetectedMatrix(frame);
+
+		// test print of matrix if changed
+		QString str;
+		str = QString::number(mat.data()[0],'g' ,1  );
+		str += " " +  QString::number(mat.data()[1], 'g', 2);
+		str += " " +  QString::number(mat.data()[2], 'g', 3);
+		qDebug() << i << ": " << str;
+		msleep(200);
 
 	}
+	cv::destroyWindow("face");
+	cvReleaseCapture( &capture );
+
 }
 
 
@@ -29,3 +77,5 @@ void ArucoThread::pause()
 {
 	this->cancel=true;
 }
+
+
