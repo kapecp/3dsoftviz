d9a2e0f827c8d440922a30e06a8457775afe1ed4
durcakd <durcak01.david@gmail.com>
2014-03-01 16:39:39 +0100

    [ArucoThread] update run method to get result from aruco as translation vector and rotation quaternion rather than matrix. Add 2 new signals for emiting them. Add computeCorQuatAndPos() method for setting correction translation vector and rotation quaternion and correctQuatAndPos() method to apply correction to actual positon vector and rotation quaternion as alternativ to computeCorMat, that use matrices. Rename member variables also. Add test code for added methods that compare result with matrixes method, but it is not implemented as test yet

--- include/Aruco/arucothread.h
+++ include/Aruco/arucothread.h
@@ -33,6 +33,21 @@ signals:
 	* @param mat ModelView matrix from aruco
 	*/
 	void pushArucoMVMat( QMatrix4x4 mat);
+
+	/**
+	* @author Dávid Durčák
+	* @brief sendArucoPosVec signal that emit vector that represent position of detected marker
+	* @param pos Position vector
+	*/
+	void sendArucoPosVec( QVector3D pos);
+
+	/**
+	* @author Dávid Durčák
+	* @brief sendArucoRorQuat signal that emit Quaternion that represent rotation of detected marker
+	* @param quat Quaternion
+	*/
+	void sendArucoRorQuat( QQuaternion quat);
+
 	/**
 	* @author Dávid Durčák
 	* @brief pushImage signal that emit image with marked marker for debuging
@@ -57,6 +72,23 @@ private:
 
 	/**
 	* @author Dávid Durčák
+	* @brief computeCorQuatAndPos Compute correction corP translation vector and corQ rotation quaternion, that are used later in correctQuatAndPost method.
+	* @param position[3] array that describe position of marker by vector (x,y,z)
+	* @param quaternion[4] array that describe orientation of marker by quaternion (w,x,y,z)
+	*/
+	void computeCorQuatAndPos( const double position[3], const double rotation[] );
+
+	/**
+	* @author Dávid Durčák
+	* @brief correctQuatAndPos Compute corrected actual position vector and rotation quaternion from correction mCorP vector and mCorQ quaternion.
+	* It is need to be done if cammera is not in horizontal direction. Result corrected parameters describe marker like camera was in horizontal direction.
+	* @param actPos input vector that  array that describe actual position of marker by vector (x,y,z)
+	* @param actQuat array that describe actual orientation of marker by quaternion (w,x,y,z)
+	*/
+	void correctQuatAndPos( QVector3D &actPos, QQuaternion &actQuat ) const;
+
+	/**
+	* @author Dávid Durčák
 	* @brief computeCorMat Compute correction corM matrix.
 	* Correction matrix == rotation around point described by origM.
 	* Applycation: corected matrix = corM * mat.   Correction matrix must be on left in multiplication.
@@ -71,7 +103,7 @@ private:
 	* @param mat Printed matrix
 	* @param name Optional name of printed matrix
 	*/
-	void printMat( const QMatrix4x4 mat, const QString name = "Matrix: ");
+	void printMat( const QMatrix4x4 mat, const QString name = "Matrix: ") const;
 
 	/**
 	* @author Dávid Durčák
@@ -79,11 +111,16 @@ private:
 	* @param v Printed vector
 	* @param name Optional name of printed vector
 	*/
-	void printVec( const QVector3D v, const QString name = "vector:  ");
+	void printVec( const QVector3D v, const QString name = "Vector:  ") const;
+	void printVec( const QVector4D v, const QString name = "Vector:  ") const;
 
 
-	bool cancel;
-	QMatrix4x4 corM;		// correction matrix
+	bool		mCancel;
+	bool		mCorSetted;	// true if mCorQ and mCorP was setted by computeCorQuatAndPos()
+	QMatrix4x4	mCorM;		// correction matrix
+	QQuaternion mCorQ;		// correction quaternion
+	QVector3D   mCorP;		// correction vector
+
 
 	};
 } // ArucoModul namespace
--- src/Aruco/arucothread.cpp
+++ src/Aruco/arucothread.cpp
@@ -10,17 +10,30 @@ using namespace ArucoModul;
 
 ArucoThread::ArucoThread(void)
 {
-	cancel=false;
+	mCancel		= false;
+	mCorSetted	= false;
 }
 
 ArucoThread::~ArucoThread(void)
 {
 }
 
+void ArucoThread::setCancel(bool set){
+	mCancel	= set;
+}
 
+void ArucoThread::pause()
+{
+	mCancel = true;
+}
 
 void ArucoThread::run()
 {
+	double	actPosArray[3];		// x, y, z
+	double	actQuatArray[4];		// angle(w), x, y, z
+	bool	marerDetected = false;
+
+
 	// this code will be changed soon
 	cv::Mat frame;
 	cv::VideoCapture capture;
@@ -44,21 +57,30 @@ void ArucoThread::run()
 	ArucoCore aCore(filename);
 
 
-	while(!cancel) {	// doing aruco work in loop
+	while(! mCancel) {	// doing aruco work in loop
 
 		capture >> frame;		// get image from camera
 
-		// add image to aruco and get matrix
-		mat = aCore.getDetectedMatrix(frame);
+		// add image to aruco and get position vector and rotation quaternion
+		marerDetected = aCore.getDetectedPosAndQuat( frame, actPosArray, actQuatArray );
+		if( marerDetected ){
+			// test if marker was detect (if not, all number in matrix are not range)
+			//if(mat.data()[ 0] > -100.0 && mat.data()[ 0] < 100.0  ){  ?????????????????
 
-		// test if marker was detect (if not, all number in matrix are not range)
-		if(mat.data()[ 0] > -100.0 && mat.data()[ 0] < 100.0  ){
-			//printMat(mat, "ArucoMatrix");
-			emit pushArucoMVMat( mat );			// emit matrix to CameraManipulation
+			QVector3D	actPos(	-actPosArray[0], -actPosArray[1], -actPosArray[2]);
+			QQuaternion actQuat( QVector4D( actQuatArray[0], actQuatArray[1], actQuatArray[2], actQuatArray[3] ));
+
+			printVec( actPos, "ActPos ");
+			printVec( actQuat.toVector4D(), "ActQuat ");
+
+			//correctQuatAndPos( actPos, actQuat );
+
+			emit sendArucoPosVec( actPos );
+			emit sendArucoRorQuat( actQuat );
+
+			//}  ?????
 		}
-
 		emit pushImage( aCore.getDetImage());	// emit image with marked marker for debuging
-
 		msleep(50);
 
 	}
@@ -67,17 +89,35 @@ void ArucoThread::run()
 
 
 
-void ArucoThread::setCancel(bool set){
-	this->cancel=set;
+
+
+void ArucoThread::computeCorQuatAndPos( const double position[3], const double rotation[] ){
+	// set corection translation
+	mCorP.setX( -position[0] );
+	mCorP.setY( -position[1] );
+	mCorP.setZ( -position[2] );
+	// set corection quaternion
+	mCorQ.setScalar( rotation[0] );
+	mCorQ.setVector( rotation[1], rotation[2], rotation[3] );
+	mCorSetted = true;
 }
 
-void ArucoThread::pause()
+void ArucoThread::correctQuatAndPos( QVector3D &actPos, QQuaternion &actQuat ) const{
+	if( mCorSetted == true ){
+		// correct position
+		// rotate point around correction point(corP) =  translate, rotate and translate back
+		actPos = mCorQ.rotatedVector( actPos - mCorP ) + mCorP;
+
+		// correct rotation
+		actQuat = mCorQ * actQuat;
+		//actQuat = actQuat * corQ ;
+	} else {
+		qDebug() << "ArucoThread:computeCorQautAndPos() was called before setted correction parameters before!";
+	}
+}
+
+void ArucoThread::computeCorMat( QMatrix4x4 origM )
 {
-	this->cancel=true;
-}
-
-
-void ArucoThread::computeCorMat( QMatrix4x4 origM ){
 
 	QMatrix4x4 T1, T2;
 
@@ -94,7 +134,7 @@ void ArucoThread::computeCorMat( QMatrix4x4 origM ){
 	origM(2,3) = 0.0;
 
 	// compute corection matrix
-	corM = T2 * origM * T1;
+	mCorM = T2 * origM * T1;
 
 	// !!! corection matrix must be on left in multiplication
 	// corM * corrected matrix
@@ -108,12 +148,18 @@ void ArucoThread::computeCorMat( QMatrix4x4 origM ){
 }
 
 
-void ArucoThread::printVec( const QVector3D v, const QString name){
+void ArucoThread::printVec( const QVector3D v, const QString name)const
+{
 	qDebug() << name << " " << v.x() << " " << v.y() << " " << v.z();
 }
 
+void ArucoThread::printVec( const QVector4D v, const QString name) const
+{
+	qDebug() << name << " " << v.x() << " " << v.y() << " " << v.z() << " " << v.w();
+}
 
-void ArucoThread::printMat( const QMatrix4x4 mat, const QString name){
+void ArucoThread::printMat( const QMatrix4x4 mat, const QString name) const
+{
 	qDebug() << name;
 	QString str;
 	str  = " " + QString::number(mat.data()[ 0], 'f', 2);
@@ -162,4 +208,97 @@ test(){
 
 	qDebug( res1 == res2 );
 }
+*/
+
+/*
+test(){
+
+			// original, nemenny------------
+			double xpos[3]	= {  0.0110871, -0.125618, 0.331737 };
+			double xor[4]	= { -0.0172416, -0.018111, 0.9853, -0.168995 };
+			QVector3D xposvk( xpos[0],  xpos[1],  xpos[2]);
+			QVector3D xposvz(-xpos[0], -xpos[1], -xpos[2]);
+			QVector4D xorv(   xor[0],   xor[1],   xor[2], xor[3] );
+			QQuaternion xquatQ(xorv);
+			QMatrix4x4 xrotM;
+			xrotM.rotate(xquatQ);
+			QMatrix4x4 xTk;
+			xTk.translate(xposvk);
+			xrotM = xTk * xrotM;
+			///printMat(xrotM, "xrotM  --- original");
+			// original matica podla ktrej sa bude robit
+
+			// --------------------------
+			// priprava aktualnej matice
+			QVector3D posvk( pos[0],  pos[1],  pos[2]);
+			QVector3D posvz(-pos[0], -pos[1], -pos[2]);
+			///printVec(posvk, "aktual pos ");
+			///qDebug() <<		"aktual quat " << or[0] << " " << or[1] << " " << or[2] << " " << or[3];
+			QVector4D orv(   or[0],   or[1],   or[2], or[3] );
+
+			QQuaternion quatQ(orv);
+			QMatrix4x4 rotM;
+			rotM.rotate(quatQ);
+			QMatrix4x4 Tk;
+			Tk.translate(posvk);
+			rotM = Tk * rotM;
+			printMat(rotM, "rotM  ---   aktual");
+
+
+
+			// ----------------------------
+			// vysledok podla matic
+			QMatrix4x4 orig = xrotM;
+			computeCorMat(orig);
+			QMatrix4x4 resM1 = corM * rotM;
+			//printMat(corM,  "corM   ---   1  medyivysledok");
+			///printMat(resM1, "resM1  ---   1  vysledok, podla matic");
+			// -----------------------
+			//=======================================
+			//=======================================
+
+			//musim vediet vypocitat 2 veci bod posunuty
+			// qatermion
+			QQuaternion corQ;		// xquatQ, xposvk, xposvz
+
+			corQ = xquatQ;
+			//corQ.slerp();
+
+
+			//------------------
+			QMatrix4x4 corQM;
+			corQM.rotate(corQ);
+			//printMat(corQM, "corQM  ---  2 medyivysledok");
+			//-------------------------------------
+
+
+			QQuaternion resQ;		// corQ, posvk, posvz, quatQ
+			QVector3D rposv;
+
+			rposv = posvk - xposvk;
+			printVec( rposv, "2 vysledok " );
+			rposv = corQ.rotatedVector(rposv);
+			//rposv = (corQM * QQuaternion(0, rposv) * corQM.conjugate()).vector();
+			printVec( rposv, "2 vysledok " );
+			rposv = rposv + xposvk;
+
+			printVec( rposv, "2 vysledok " );
+
+			resQ = corQ*quatQ;
+			//resQ = quatQ*corQ;
+
+
+			//------------------------------------
+
+			QMatrix4x4 resQM;
+			resQM.rotate(resQ);
+			printMat(resM1, "resM1  ---   1  vysledok, podla matic");
+			QMatrix4x4 rTk;
+			rTk.translate(rposv);
+			resQM = rTk * resQM;
+			printMat(resQM, "resQM  ---   2 aktual pre qaternion");
+
+
+}
+
 */
