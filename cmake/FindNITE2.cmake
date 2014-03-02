00e910f1e987ec42a5c3af3687530c95155b88ab
DanielSoos <soos.dano@gmail.com>
2014-03-02 21:20:55 +0100

    Added dll files to _install/bin
    To work with OpenNI2 and NiTE2 the addition of dll files was needed
    to /_install/bin dir
    
    - Edited CmakeLists.txt to correspond OpenNI2 and NiTE2 with Win
    - CMAKE_INCLUDE_PATH needs to be updated with the path of the needed
    .dll files - if NiTE is installec correctly it is in the /Samples/Bin/
    /OpenNI2/Drivers dir
    - Tested with OpenNI2.0 and NiTE 2.2

--- cmake/FindNITE2.cmake
+++ cmake/FindNITE2.cmake
@@ -7,6 +7,7 @@
 #=============================================================================
 if (WIN32)
   find_path( NITE2_INCLUDE_DIR NAMES NiTe.h )
+  find_path( OPENNI2_DLL NAMES Kinect.dll )
 	
   find_library( NITE2_LIBRARY NAMES NiTe2 )
 endif ()
@@ -14,12 +15,13 @@ endif ()
 # handle the QUIETLY and REQUIRED arguments and set NITE2_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(NITE2 REQUIRED_VARS NITE2_LIBRARY NITE2_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(NITE2 REQUIRED_VARS NITE2_LIBRARY NITE2_INCLUDE_DIR OPENNI2_DLL)
 
 # Copy the results to the output variables.
 if(NITE2_FOUND)
   set(NITE2_LIBRARIES ${NITE2_LIBRARY})
   set(NITE2_INCLUDE_DIRS ${NITE2_INCLUDE_DIR})
+  set(NITE2_OPENNI2_DRIVER_DIRS ${OPENNI2_DLL})
 endif()
 
-mark_as_advanced(NITE2_INCLUDE_DIR NITE2_LIBRARY)
+mark_as_advanced(NITE2_INCLUDE_DIR NITE2_LIBRARY OPENNI2_DLL)
