63b00cef0f72bf9d08d0b6708e1d169d8f688e4d
marconak <marconak@gmail.com>
2014-03-20 20:45:14 +0100

    [Kinect] edit Thread for testing kinect window and testing conversion

--- include/Kinect/KinectThread.h
+++ include/Kinect/KinectThread.h
@@ -21,6 +21,7 @@ public:
 
 	void run();
 
+
 signals:
 
 	void pushImage(cv::Mat image);
--- src/Kinect/KinectThread.cpp
+++ src/Kinect/KinectThread.cpp
@@ -1,13 +1,15 @@
 
 #include "Kinect/KinectThread.h"
 #include "Kinect/KinectCore.h"
+#include "Kinect/KinectRecognition.h"
 #include "QDebug"
 
 //only for testing
 #include "opencv/highgui.h"
 #include <opencv2/core/core.hpp>
 #include "opencv2/opencv.hpp"
-
+#include "OpenNI.h"
+#include "NiTE.h"
 
 
 using namespace Kinect;
@@ -38,36 +40,51 @@ void Kinect::KinectThread::run()
 {
 
 	mCancel=false;
-
+	/////Class kinect recognition
+	//Kinect::KinectRecognition *mKinect = new Kinect::KinectRecognition();
+	//openni::VideoStream  color;
+	/////////end////////////
 
 	cv::Mat frame;
 
-	//KinectCore kCore;
-
-	//temporary for testing
+	//////////////for Testing
 	cv::VideoCapture cap(0);
 	if(!cap.isOpened())
 	{
 		qDebug() << "No camera";
 		exit();
 	}
+	//////////////Koniec testovania//////////////////
 
+	//////////////Kinect ///////
+	//	color.create(mKinect->device, openni::SENSOR_COLOR);
+	//	color.start();
+	//	openni::VideoFrameRef colorFrame;
+
+	//nite::HandTracker handTracker;
+	//handTracker.create();
+	//nite::HandTrackerFrameRef handTrackerFrame;
 
 	while(!mCancel)
 	{
+		//color.readFrame(&colorFrame);
+		//frame=mKinect->colorImageCvMat(colorFrame);
+
+		///////////////Dept test///////////
+		//handTracker.readFrame(&handTrackerFrame);
+		//frame = mKinect->depthImageCvMat(handTrackerFrame);
+		//////////////End/////////////
 
 		cap >> frame; // get a new frame from camera
-
 		cv::cvtColor(frame, frame, CV_BGR2RGB);
 		//QImage qimage ( (uchar*) image.data, image.cols, image.rows,(int) image.step, QImage::Format_RGB888);
 
+		///////Emit Qimage and Image////////////
 		//emit pushImage( qimage );
 		emit pushImage( frame );
-		//cv::imshow("daco",frame);
-		//cv::waitKey(10);
 
 	}
-	//testing
+	//testing Camera//////////////
 	cap.release();
 
 }
