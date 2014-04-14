dd31d04cd449521da7f4a71f88b16da4397c2a40
durcakd <durcak.david01@gmail.com>
2014-04-14 21:02:33 +0200

    [CameraStream] fix compilation problems on Win32 platform with creating black cv::Mat and fix conversion cv::Mat to osg::Image.

--- src/Viewer/CameraStream.cpp
+++ src/Viewer/CameraStream.cpp
@@ -12,19 +12,23 @@ CameraStream::CameraStream(osg::Geometry *geom) : QObject(), osg::Image()
 	mWidth	= 0;
 	mHeight = 0;
 
-    cv::Mat cvImg(cv::Mat::zeros(480,640, CV_8UC3));
+	cv::Mat cvImg(480,640, CV_8UC3, CV_RGB(0,0,0));
 
-    updateBackgroundImage( cvImg);
+	updateBackgroundImage( cvImg);
 }
 
 CameraStream::~CameraStream(){}
 
 void CameraStream::updateBackgroundImage( cv::Mat cvImg)
 {
+	if(cvImg.empty()) {
+		qDebug() << "CameraStream::updateBackgroundImage(): warning, cvImg is empty!";
+		return;
+	}
+
 	// update geometry coordinates if thare are different dimensions of image,
 	// becasuse probebly changed it ratio of sides
-    if(!cvImg.empty()){
-    if(mGeom != NULL ){
+	if(mGeom != NULL ){
 		if( cvImg.cols != mWidth || mHeight != cvImg.rows){
 			mWidth	= cvImg.cols;
 			mHeight = cvImg.rows;
@@ -32,26 +36,30 @@ void CameraStream::updateBackgroundImage( cv::Mat cvImg)
 		}
 	}
 
-    //IplImage* iplImg = cvCloneImage( &(IplImage)cvImg);
+	// There will be probably needed refactoring on MAC OS
+#ifdef WIN32
+	IplImage *iplImg = cvCloneImage( &(IplImage)cvImg);
 
+	setImage( iplImg->width, iplImg->height,
+			  3, GL_RGB, GL_RGB,
+			  GL_UNSIGNED_BYTE,
+			  (unsigned char*) iplImg->imageData,
+			  NO_DELETE, 1);
+#else
+	setImage(cvImg.cols, cvImg.rows, 3,
+			 GL_RGB, GL_RGB, GL_UNSIGNED_BYTE,
+			 (unsigned char*) cvImg.data,
+			 NO_DELETE, 1);
+#endif
 
-
-    setImage(cvImg.cols, cvImg.rows, 3,
-              GL_RGB, GL_RGB, GL_UNSIGNED_BYTE,
-              (cvImg.data),
-              NO_DELETE, 1);
-    }
-   dirty();
+	dirty();
 
 }
 
 void CameraStream::updateGeometryCoords(int width, int height)
 {
-	//qDebug() << width << " x " << height;
 	float x;
 	x = ((float)width) / ((float)height);
-
-	//qDebug() << "x " << x;
 
 	osg::Vec3Array* coords = (osg::Vec3Array*) mGeom->getVertexArray();
 	(*coords)[0].set( -x, 1.5f, -1.0f );
