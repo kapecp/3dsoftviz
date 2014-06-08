671e7cac043d927254bcd37548a19f7a222361e5
Frantisek Nagy <xnagyf@is.stuba.sk>
2014-06-08 03:21:51 +0200

    comments

--- include/LuaGraph/FullHyperGraphVisualizer.h
+++ include/LuaGraph/FullHyperGraphVisualizer.h
@@ -5,9 +5,23 @@
 
 namespace Lua {
 
+/**
+     * \class FullHyperGraphVisualizer
+     * \brief Implementation of LuaGraphVisualizer that draws incidences as nodes in graph
+     *
+     * \author Frantisek Nagy
+     * \date 19.5.2014
+     */
 class FullHyperGraphVisualizer : public Lua::LuaGraphVisualizer
 {
 public:
+    /**
+         * \fn FullHyperGraphVisualizer
+         * \brief constructor
+         *
+         * \params graph    Data::Graph object to fill
+         * \params camera   camera for edges to face
+         */
     FullHyperGraphVisualizer(Data::Graph *graph, osg::Camera *camera = NULL);
 
     void visualize();
--- include/LuaGraph/HyperGraphVisualizer.h
+++ include/LuaGraph/HyperGraphVisualizer.h
@@ -5,9 +5,23 @@
 
 namespace Lua {
 
+/**
+     * \class HyperGraphVisualizer
+     * \brief Implementation of LuaGraphVisualizer that draws edges as nodes in graph
+     *
+     * \author Frantisek Nagy
+     * \date 19.5.2014
+     */
 class HyperGraphVisualizer : public Lua::LuaGraphVisualizer
 {
 public:
+    /**
+         * \fn HyperGraphVisualizer
+         * \brief constructor
+         *
+         * \params graph    Data::Graph object to fill
+         * \params camera   camera for edges to face
+         */
     HyperGraphVisualizer(Data::Graph *graph, osg::ref_ptr<osg::Camera> camera = NULL);
 
     void visualize();
