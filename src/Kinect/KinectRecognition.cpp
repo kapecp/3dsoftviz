c63d93b86f9205cd82ccabcdf8f708353e4ae2a8
marconak <marconak@gmail.com>
2014-06-14 15:45:30 +0200

    [Kinect] base comments to cpp

--- src/Kinect/KinectRecognition.cpp
+++ src/Kinect/KinectRecognition.cpp
@@ -11,16 +11,18 @@ Kinect::KinectRecognition::KinectRecognition()
 void Kinect::KinectRecognition::inicialize()
 {
 	isOpen=true;
+
+	//initialize OpenNI
 	mOpenniStatus=openni::OpenNI::initialize();
-	if(mOpenniStatus != openni::STATUS_OK)
+	if(mOpenniStatus != openni::STATUS_OK) // check
 	{
 		qDebug() << "Openni initialization failed";
 		isOpen=false;
 	}
 
+	// open device - here we can select specific device - in Microsoft Kinect 0,1,...
 	mOpenniStatus=device.open(openni::ANY_DEVICE);
-//	openni::DeviceInfo devinf;
-//	devinf.getUri();
+	//check
 	if(mOpenniStatus != openni::STATUS_OK)
 	{
 		qDebug() << "Openni Device not Open";
@@ -30,21 +32,25 @@ void Kinect::KinectRecognition::inicialize()
 
 void Kinect::KinectRecognition::closeOpenni()
 {
+	// close device and shutdown Openni
 	device.close();
 	openni::OpenNI::shutdown();
 }
 
 Kinect::KinectRecognition::~KinectRecognition()
 {
+	device.close();
 	openni::OpenNI::shutdown();
 }
 
 cv::Mat Kinect::KinectRecognition::colorImageCvMat(openni::VideoFrameRef &colorFrame)
 {
 	cv::Mat frame;
+	// read specific data from color frame
 	const openni::RGB888Pixel* imageBuffer = (const openni::RGB888Pixel*)colorFrame.getData();
-
+	// create cv mat frame
 	frame.create(colorFrame.getHeight(), colorFrame.getWidth(), CV_8UC3);
+	// copy data
 	memcpy( frame.data, imageBuffer, 3*colorFrame.getHeight()*colorFrame.getWidth()*sizeof(uint8_t) );
 
 	return frame;
@@ -52,6 +58,7 @@ cv::Mat Kinect::KinectRecognition::colorImageCvMat(openni::VideoFrameRef &colorF
 
 cv::Mat Kinect::KinectRecognition::depthImageCvMat(openni::VideoFrameRef &colorFrame)
 {
+	//TODO check functionality
 	openni::VideoMode mode = colorFrame.getVideoMode();
 	cv::Mat image = cv::Mat(mode.getResolutionY(), mode.getResolutionX(), CV_8UC3, (char*)colorFrame.getData());
 	return image;
@@ -59,9 +66,11 @@ cv::Mat Kinect::KinectRecognition::depthImageCvMat(openni::VideoFrameRef &colorF
 
 QImage Kinect::KinectRecognition::colorImageQImage(openni::VideoFrameRef &colorFrame)
 {
+	// create QImage from videframeref
 	QImage image = QImage((uchar*)colorFrame.getData(),
 						  colorFrame.getWidth(), colorFrame.getHeight(),
-						  QImage::Format_RGB888);
+						  QImage::Format_RGB888); // format color
+	// rgb swapped
 	image = image.rgbSwapped();
 	return image;
 
@@ -72,7 +81,8 @@ QImage Kinect::KinectRecognition::deptImageQImage(openni::VideoFrameRef &colorFr
 	cv::Mat depthMat = depthImageCvMat(colorFrame);
 	QImage image = QImage(depthMat.data,
 						  colorFrame.getWidth(), colorFrame.getHeight(),
-						  QImage::Format_Indexed8);
+						  QImage::Format_Indexed8); // color white black
+	// convert to format
 	return image.convertToFormat(QImage::Format_RGB32);
 }
 
