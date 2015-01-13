300d14516b2df2872639147eb6f6f5cc273a56f6
Peter Kapec <kapecp@gmail.com>
2015-01-13 20:52:29 +0100

    [Build] tweaks to make Kinect build available on Linux
    [-] removed WIN32 build restrictions
    [!] FindOPENNI2.cmake and FindNITE2.cmake
    	Work-In-Progres:
    		- simple hardcoded search paths
    		- Kinect.dll ignored

--- cmake/FindNITE2.cmake
+++ cmake/FindNITE2.cmake
@@ -5,23 +5,31 @@
 #  NITE2_LIBRARIES    - List of libraries when using NiTe.
 #  NITE2_FOUND        - True if NiTe found.
 #=============================================================================
-if (WIN32)
-	find_path( NITE2_INCLUDE_DIR NAMES NiTe.h )
-	find_path( OPENNI2_DLL NAMES Kinect.dll )
 
-	find_library( NITE2_LIBRARY NAMES NiTe2 )
-endif ()
+find_path( NITE2_INCLUDE_DIR
+	NAMES NiTE.h
+	HINTS /home/kapec/Desktop/work-in-progress/NiTE-Linux-x64-2.2/Include
+	)
+#find_path( OPENNI2_DLL NAMES Kinect.dll)
+
+find_library( NITE2_LIBRARY
+	NAMES NiTE2
+	HINTS /home/kapec/Desktop/work-in-progress/NiTE-Linux-x64-2.2/Redist
+	)
+
 
 # handle the QUIETLY and REQUIRED arguments and set NITE2_FOUND to TRUE if
 # all listed variables are TRUE
 include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( NITE2 REQUIRED_VARS NITE2_LIBRARY NITE2_INCLUDE_DIR OPENNI2_DLL )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( NITE2 REQUIRED_VARS NITE2_LIBRARY NITE2_INCLUDE_DIR #OPENNI2_DLL
+ )
 
 # Copy the results to the output variables.
 if( NITE2_FOUND )
 	set( NITE2_LIBRARIES ${NITE2_LIBRARY} )
 	set( NITE2_INCLUDE_DIRS ${NITE2_INCLUDE_DIR} )
-	set( NITE2_OPENNI2_DRIVER_DIRS ${OPENNI2_DLL} )
+	#set( NITE2_OPENNI2_DRIVER_DIRS ${OPENNI2_DLL} )
 endif()
 
-mark_as_advanced( NITE2_INCLUDE_DIR NITE2_LIBRARY OPENNI2_DLL )
+mark_as_advanced( NITE2_INCLUDE_DIR NITE2_LIBRARY #OPENNI2_DLL
+	)
