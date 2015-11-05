79bb30b76d168737f2f4c78de42fcd324f7f37c6
jagazdik <jaro260@gmail.com>
2015-11-05 14:57:28 +0100

    Removed WIN32 only header

--- cmake/Find3DMOUSE.cmake
+++ cmake/Find3DMOUSE.cmake
@@ -4,16 +4,14 @@
 #  3DMOUSE_LIBRARIES    - List of libraries when using 3DMOUSE.
 #  3DMOUSE_FOUND        - True if 3DMOUSE found.
 #=============================================================================
-if( WIN32 )
-	find_path( 3DMOUSE_INCLUDE_DIR
-		NAMES si.h siapp.h spwmath.h
-		PATHS $ENV{3DMOUSE_DIR}/Inc
-	)
-	find_library( 3DMOUSE_LIBRARY
-		NAMES siapp spwmath
-		PATHS $ENV{3DMOUSE_DIR}/Lib/x86
-	)
-endif()
+find_path( 3DMOUSE_INCLUDE_DIR
+	NAMES si.h
+	PATHS $ENV{3DMOUSE_DIR}/Inc
+)
+find_library( 3DMOUSE_LIBRARY
+	NAMES siapp
+	PATHS $ENV{3DMOUSE_DIR}/Lib/x86
+)
 
 include( FindPackageHandleStandardArgs )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( 3DMOUSE REQUIRED_VARS 3DMOUSE_LIBRARY 3DMOUSE_INCLUDE_DIR )
