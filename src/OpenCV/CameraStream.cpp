813ffe2b7b807fa926545184952a674d31c19ac3
Denis Illés <denis.illes@gmail.com>
2016-03-09 22:00:50 +0100

    fixed coversion cv::Mat to IplImage*

--- src/OpenCV/CameraStream.cpp
+++ src/OpenCV/CameraStream.cpp
@@ -18,8 +18,9 @@ CameraStream::CameraStream( osg::Geometry* geom ) :
 #ifdef WIN32
 	cv::Mat cvImg( 480,640, CV_8UC3, CV_RGB( 0,0,0 ) ); // Black on Win
 #else
-	cv::Mat cvImg;			// gray on Linux
+	cv::Mat cvImg;			// gray on Linux	
 #endif
+
 	updateBackgroundImage( cvImg );
 
 }
@@ -33,12 +34,12 @@ void CameraStream::updateBackgroundImage( cv::Mat cvImg )
 		return;
 	}
 
-
 	if ( cvImg.cols != mWidth || mHeight != cvImg.rows ) {
 		mWidth	= cvImg.cols;
 		mHeight = cvImg.rows;
 #ifdef WIN32
-		iplImg = cvCloneImage( ( IplImage* )&cvImg );
+		//iplImg = cvCloneImage( ( IplImage* )cvImg );
+		iplImg=cvCloneImage(&(IplImage)cvImg);
 #endif
 		// update geometry coordinates if thare are different dimensions of image,
 		// becasuse probebly changed it ratio of sides
@@ -50,7 +51,7 @@ void CameraStream::updateBackgroundImage( cv::Mat cvImg )
 
 	// There will be probably needed refactoring on MAC OS
 #ifdef WIN32
-	cvCopy( ( IplImage* )&cvImg, iplImg, NULL );
+	cvCopy( &( IplImage )cvImg, iplImg, NULL );
 
 	setImage( iplImg->width, iplImg->height,
 			  3, GL_RGB, GL_RGB,
