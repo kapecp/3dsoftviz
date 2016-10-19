5a19da0cf2b73971a94c338084b628e2ca03c58d
Peter Kapec <kapecp@gmail.com>
2016-10-19 20:23:04 +0200

    [Build] Add initial StackTracePlus support for better Lua stacktrace reports

--- resources/scripts/app/main.lua
+++ resources/scripts/app/main.lua
@@ -1,3 +1,22 @@
 function_call_graph = require "softviz/function_call_graph"
 logical_filter = require "softviz/logical_filter"
 slt2_renderer = require "softviz/slt2_renderer"
+
+local STP = require "StackTracePlus"
+debug.traceback = STP.stacktrace
+
+--[[
+function test()
+    local s = "this is a string"
+    local n = 42
+    local t = { foo = "bar" }
+    local co = coroutine
+    local cr = coroutine.create
+
+    local t = {}
+    local a=nil
+    --print(t.b.c)
+    error("an error")
+end
+test()
+--]]
