5d6915d7de688561b89ffbf15a98809ea07384a1
Peter Kapec <kapecp@gmail.com>
2016-10-19 21:25:24 +0200

    [Build] Add mobdebug submodule, support for remote debugging of Lua code

--- resources/scripts/app/main.lua
+++ resources/scripts/app/main.lua
@@ -2,10 +2,13 @@ function_call_graph = require "softviz/function_call_graph"
 logical_filter = require "softviz/logical_filter"
 slt2_renderer = require "softviz/slt2_renderer"
 
+
+-- support for better stac trace repports
 local STP = require "StackTracePlus"
 debug.traceback = STP.stacktrace
 
 --[[
+-- simple test to test StackTracePlus
 function test()
     local s = "this is a string"
     local n = 42
@@ -20,3 +23,7 @@ function test()
 end
 test()
 --]]
+
+-- singleton for remote debbuging with ZeroBrane Studio
+theDebugger = require("mobdebug")
+theDebugger.start() 
