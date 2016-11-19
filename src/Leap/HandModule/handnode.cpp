8d4b48f337b653512e5be48cc35db79ae8b72b20
Patrik Berger <skcibi@gmail.com>
2016-11-19 13:48:44 +0100

    [ADD] #4931 Kosti pridane do modelu, refaktoring.

--- src/Leap/HandModule/handnode.cpp
+++ src/Leap/HandModule/handnode.cpp
@@ -8,16 +8,4 @@ Leap::HandNode::HandNode(){}
 
 void Leap::HandNode::initStructure(){}
 
-void Leap::HandNode::generateGeometry(float radius) {
-    osg::ref_ptr<osg::Geode> handGeode( new osg::Geode );
-    osg::ref_ptr<osg::Sphere> handSphere = new osg::Sphere( osg::Vec3f( 0.0f,0.0f,0.0f ), radius );
-
-    osg::ref_ptr<osg::ShapeDrawable> handDrawable( new osg::ShapeDrawable( handSphere.get() ) );
-    handGeode->addDrawable( handDrawable.get() );
-
-    this->addChild( handGeode.get() );
-
-
-
-
-}
+void Leap::HandNode::generateGeometry(float radius) {}
--- src/Leap/HandModule/joint.cpp
+++ src/Leap/HandModule/joint.cpp
@@ -7,21 +7,29 @@
 const float RADIUS = 0.07f;
 
 Leap::Joint::Joint(int level, int position, osg::ref_ptr<osg::Group> fingerJointGroup) {
-    fingerJointGroup->addChild(dynamic_cast<osg::Node*>(this));
     this->level = level;
     this->position = position;
     this->nextJoint = NULL;
     this->fingerJointGroup = fingerJointGroup;
     this->generateGeometry(RADIUS);
-    initStructure();
     this->fingerJointGroup->addChild(static_cast<osg::Node*> (this));
+    initStructure();  
 }
 
 void Leap::Joint::initStructure() {
-
     if (this->nextJoint == NULL) {
         if (this->level != 4) {
             this->nextJoint = new Joint(this->level + 1, this->position, this->fingerJointGroup);
         }
     }
+}
+
+void Leap::Joint::generateGeometry(float radius) {
+    osg::ref_ptr<osg::Geode> handGeode( new osg::Geode );
+    osg::ref_ptr<osg::Sphere> handSphere = new osg::Sphere( osg::Vec3f( 0.0f,0.0f,0.0f ), radius );
+
+    osg::ref_ptr<osg::ShapeDrawable> handDrawable( new osg::ShapeDrawable( handSphere.get() ) );
+    handGeode->addDrawable( handDrawable.get() );
+
+    this->addChild( handGeode.get() );
 }
