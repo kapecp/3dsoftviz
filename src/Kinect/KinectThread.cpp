910bbc671ab69ca6bd183674c5d04be9b5d5838b
RostarMarek <rostarmarek@gmail.com>
2016-12-05 14:33:02 +0100

    [FIX] 5078 Opravenie toku dat do Nite

--- src/Kinect/KinectThread.cpp
+++ src/Kinect/KinectThread.cpp
@@ -182,7 +182,7 @@ void Kinect::KinectThread::run()
 			frame=mKinect->colorImageCvMat( colorFrame );
 			cv::cvtColor( frame, frame, CV_BGR2RGB );
 			m_depth.readFrame( &depthFrame );
-
+			depth = mKinect->depthImageCvMat( depthFrame );
 			//if set true, it will capture the first frame of kinect stream and save color frame, depth frame and depth matrix in to specific location
 			if ( captureImage ) {
 
@@ -220,6 +220,7 @@ void Kinect::KinectThread::run()
 				fout.close();
 				captureImage =  false;
 			}
+
 
 #ifdef NITE2_FOUND
 			//set parameters for changes movement and cursor
