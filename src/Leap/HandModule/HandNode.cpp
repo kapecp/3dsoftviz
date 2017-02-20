80f18f6b3ac66af2412648fd30d5c69f19259651
RostarMarek <rostarmarek@gmail.com>
2017-02-20 20:20:11 +0100

    [FIX]Included necessary dependecies

--- src/Leap/HandModule/HandNode.cpp
+++ src/Leap/HandModule/HandNode.cpp
@@ -3,6 +3,7 @@
  */
 #include <easylogging++.h>
 #include "Leap/HandModule/HandNode.h"
+#include <osg/ShapeDrawable>
 
 const osg::Vec4f BLUE = osg::Vec4f( 21.0/255.0,51.0/255.0,252.0/255.0,1.0 );
 const osg::Vec4f GREEN = osg::Vec4f( 23.0/255.0,190.0/255.0,40.0/255.0,1.0 );
