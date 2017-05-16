32e077b520cd26ead4e789f25b9669e9264f7f1b
Peter Kapec <kapecp@gmail.com>
2017-05-16 12:54:56 +0200

    [FIX] Some include problems

--- include/Leap/HandModule/Manipulator/HandMapper.h
+++ include/Leap/HandModule/Manipulator/HandMapper.h
@@ -4,6 +4,8 @@
 #include "Leap.h"
 #include <opencv2/core/core.hpp>
 
+#include <osg/Vec3>
+
 namespace Vwr {
 class CoreGraph;
 }
@@ -20,19 +22,19 @@ public:
 	~HandMapper();
 
 
-    Leap::Vector recalculateDepthNode(Leap::Vector vector, float diff);
-    void setNodeScreenCoords(osg::Vec3 nodeScreenCoords);
+	Leap::Vector recalculateDepthNode(Leap::Vector vector, float diff);
+	void setNodeScreenCoords(osg::Vec3 nodeScreenCoords);
 
 
-private:  
-    float calculateAveragePalmFingerDistance(cv::vector<cv::Point> pointList,
-                                             int imageWidth, int imageHeight);
-    cv::vector<cv::Point> getHandPointList(cv::vector<cv::vector<cv::Point>> contourPointList);
-    Vwr::CoreGraph* coreGraph;   
-    cv::Point cameraOffset;
-    osg::Vec3 nodeScreenCoords;
-    unsigned long calibrationCounter;
-    cv::Point calibrationLastPoint;
+private:
+	float calculateAveragePalmFingerDistance(cv::vector<cv::Point> pointList,
+											 int imageWidth, int imageHeight);
+	cv::vector<cv::Point> getHandPointList(cv::vector<cv::vector<cv::Point>> contourPointList);
+	Vwr::CoreGraph* coreGraph;
+	cv::Point cameraOffset;
+	osg::Vec3 nodeScreenCoords;
+	unsigned long calibrationCounter;
+	cv::Point calibrationLastPoint;
 
 };
 }
