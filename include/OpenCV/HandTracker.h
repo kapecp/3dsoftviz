82a93a47b626e6b8aa522acf08039768dbd740d3
Groosling <martin.mokry@gmail.com>
2017-06-07 22:16:03 +0200

    [REF] komenty leap tried

--- include/OpenCV/HandTracker.h
+++ include/OpenCV/HandTracker.h
@@ -16,10 +16,40 @@ public:
 	HandTracker();
 
 	~HandTracker();
+    /**
+     * @brief dist calculate distance between two points
+     * @param x
+     * @param y
+     * @return
+     */
 	double dist( cv::Point x,cv::Point y );
+    /**
+     * @brief circleFromPoints - This function returns the radius and the center of the circle given 3 points
+     * @param p1
+     * @param p2
+     * @param p3
+     * @return
+     */
 	std::pair<cv::Point,double> circleFromPoints( cv::Point p1, cv::Point p2, cv::Point p3 );
+    /**
+     * @brief getParameterValues - calculates new distance from camera, based on old one track by leap sensor
+     * @param threshold
+     * @param areaSize
+     * @param brightness
+     * @param depth
+     */
 	void getParameterValues( int* threshold, int* areaSize, float brightness, float depth );
+    /**
+     * @brief produceBinaries - return greyscale image from RGB image
+     * @param m - mat of image
+     * @return
+     */
 	cv::Mat produceBinaries( cv::Mat m );
+    /**
+     * @brief findHand - finds center of the palm and radsiu of the palm
+     * @param mask
+     * @return
+     */
 	cv::vector<std::pair<cv::Point,double>> findHand( cv::Mat mask );
 
 
